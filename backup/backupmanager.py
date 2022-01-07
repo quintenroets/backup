23521ec65f0070dabbbdcbfe346d826c84f34732
Quinten <quinten.roets@gmail.com>
2022-01-07 19:19:54 +0100

    noop

--- backup/backupmanager.py
+++ backup/backupmanager.py
@@ -123,7 +123,7 @@ class BackupManager:
     def status(reverse=False):
         ProfileManager.save_active()
         BackupManager.check_cache_existence()
-        filters = BackupManager.get_filters()        
+        filters = BackupManager.get_filters()
         src, dst = (Path.HOME, Path.backup_cache) if not reverse else (Path.backup_cache, Path.HOME)
         status = Backup.compare(src, dst, filters=filters) if filters else []
         if filters and not status and not reverse:

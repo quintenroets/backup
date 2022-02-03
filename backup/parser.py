cdcf5d61ea04d7409d4c3b54365aac6f2b5de544
Quinten <quinten.roets@gmail.com>
2022-02-03 23:25:02 +0100

    special parsing for drive folder

--- backup/parser.py
+++ backup/parser.py
@@ -2,21 +2,23 @@ from .path import Path
 
 
 def parse_paths(structure):
-    tuples = parse_paths_comb(structure, {}, root=Path(''))
+    tuples = parse_paths_comb(structure, {}, root=Path(""))
     return [t[0] for t in tuples]
 
 
-def make_filters(includes=[], excludes=[], recursive=True, include_others=False, root=Path.HOME):
-    mapping = {'+': includes, '-': excludes}
+def make_filters(
+    includes=None, excludes=None, recursive=True, include_others=False, root=Path.HOME
+):
+    mapping = {"+": includes or [], "-": excludes or []}
     filters = []
     for symbol, paths in mapping.items():
         for path in paths:
-            addition = '/**' if recursive and (root / path).is_dir() else ''
-            filter = f'{symbol} /{path}{addition}'
+            addition = "/**" if recursive and (root / path).is_dir() else ""
+            filter = f"{symbol} /{path}{addition}"
             filters.append(filter)
 
     if include_others:
-        filters.append('+ **')
+        filters.append("+ **")
 
     return filters
 
@@ -27,19 +29,20 @@ def parse_paths_comb(include, exclude, root=None):
 
     while todo:
         include, exclude = todo.pop(0)
-        paths += [(it, True) for it in include.items] + [(it, False) for it in exclude.items]
+        paths += [(it, True) for it in include.items] + [
+            (it, False) for it in exclude.items
+        ]
         todo += [
             tuple(s.structures.get(subroot, Structure([])) for s in (include, exclude))
             for subroot in (include.structures | exclude.structures)
         ]
-        
     return paths[::-1]
 
 
 class Structure:
     def __init__(self, items, root=None):
         if root is None:
-            root = Path('')
+            root = Path("")
 
         self.items = []
         self.structures = {}
@@ -47,12 +50,18 @@ class Structure:
         for item in items:
             if not isinstance(item, dict):
                 item = {item: []}
-            
+
             name, subitems = next(iter(item.items()))
-            name, *parts = name.split('/')
+
+            name, *parts = name.split("/")
             subroot = root / name
-            if parts: # properly set path after / to sublevel
-                subitems = [{'/'.join(parts): subitems}]
+            drive = Path.docs / "Drive"  # special treatment needed for zip efficiency
+
+            if Path.HOME / subroot == drive:
+                subitems = [f.name for f in drive.iterdir()]
+
+            if parts:  # properly set path after / to sublevel
+                subitems = [{"/".join(parts): subitems}]
             if subitems:
                 self.structures[name] = Structure(subitems, subroot)
             else:

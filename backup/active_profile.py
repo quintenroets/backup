e19793698296053ac1b1a9ab2de8d31c0c85197f
Quinten <quinten.roets@gmail.com>
2022-02-25 16:02:00 +0100

    strip name of active profile

--- backup/active_profile.py
+++ backup/active_profile.py
@@ -4,7 +4,7 @@ from .path import Path
 class ActiveProfile:
     @property
     def name(self):
-        return Path.active_profile.text or "light"
+        return Path.active_profile.text.strip() or "light"
 
     @name.setter
     def name(self, name):

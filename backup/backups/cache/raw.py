04e2a806bf0bd91d2f62a156b63e49706d093839
Quinten <quinten.roets@gmail.com>
2023-09-29 19:16:37 +0200

    explicitly ignore dest path

--- backup/backups/cache/raw.py
+++ backup/backups/cache/raw.py
@@ -7,3 +7,8 @@ from ...utils import Path
 @dataclass
 class Backup(backup.Backup):
     dest: Path = Path.backup_cache
+
+    def generate_path_rules(self):
+        dest_pattern = Backup.dest.relative_to(self.original_source)
+        yield f"- /{dest_pattern}/**"
+        yield from super().generate_path_rules()

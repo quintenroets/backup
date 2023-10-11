7ca7875bb6f418352b68a72997f460bc9cade568
Quinten <quinten.roets@gmail.com>
2023-10-11 07:28:11 -0700

    fix exclude dest filter for subcheck

--- backup/backups/cache/raw.py
+++ backup/backups/cache/raw.py
@@ -9,8 +9,9 @@ class Backup(backup.Backup):
     dest: Path = Path.backup_cache
 
     def generate_path_rules(self):
-        dest_pattern = Backup.dest.relative_to(self.original_source)
-        yield f"- /{dest_pattern}/**"
+        if Backup.dest.is_relative_to(self.original_source):
+            dest_pattern = Backup.dest.relative_to(self.original_source)
+            yield f"- /{dest_pattern}/**"
         if not self.paths:
             yield "+ *"
         yield from super().generate_path_rules()

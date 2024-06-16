b6a52352cb1e2d998fc154ddef036390ea14868a
Quinten <quinten.roets@gmail.com>
2024-06-16 11:51:25 -0700

    make coverage more robust

---
+++ tests/cache/test_detailed_entry.py
@@ -0,0 +1,36 @@
+from unittest.mock import MagicMock, patch
+
+from backup.backups.cache.detailed_entry import Entry
+from backup.context import Context
+
+
+@patch("cli.capture_output_lines", return_value=[""])
+@patch("xattr.xattr.set")
+def test_detailed_checker(_: MagicMock, __: MagicMock, test_context: Context) -> None:
+    path = test_context.profiles_source_root / ".config" / "gtkrc"
+    entry = Entry(
+        source_root=test_context.config.backup_source,
+        dest_root=test_context.config.backup_dest,
+        source=path,
+    )
+    path.touch()
+    entry.is_changed()
+    path.text = " "
+    path.touch()
+
+
+@patch("cli.capture_output_lines", return_value=[""])
+@patch("xattr.xattr.set")
+def test_detailed_checker_hash_path(
+    _: MagicMock, __: MagicMock, test_context: Context
+) -> None:
+    path = test_context.profiles_source_root / ".config" / "rclone" / "rclone.conf"
+    path.touch()
+    entry = Entry(
+        source_root=test_context.config.backup_source,
+        dest_root=test_context.config.backup_dest,
+        source=path,
+    )
+    entry.is_changed()
+    path.text = " "
+    entry.is_changed()

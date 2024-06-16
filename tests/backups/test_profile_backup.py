1e12109177dfb96689a63cd8954d729866fb5f24
Quinten <quinten.roets@gmail.com>
2024-06-16 15:51:09 -0700

    add more checks to test

--- tests/backups/test_profile_backup.py
+++ tests/backups/test_profile_backup.py
@@ -1,7 +1,6 @@
-import collections
-
 from backup import Backup
 from backup.backups import profile
+from backup.models import Path
 
 
 def test_set_profile(mocked_backup: Backup) -> None:
@@ -10,11 +9,13 @@ def test_set_profile(mocked_backup: Backup) -> None:
 
 def test_generate_path_rules(mocked_backup: Backup) -> None:
     backup = profile.Backup()
+    directory = Path("dummy_directory")
+    expected_paths = (directory / "out.txt", directory / "out2.txt")
     paths_to_create = (
-        backup.source / "dummy_directory" / "out.txt",
-        backup.dest / "dummy_directory" / "out2.txt",
+        backup.source / expected_paths[0],
+        backup.dest / expected_paths[1],
     )
     for path in paths_to_create:
         path.touch()
-    paths = backup.generate_paths()
-    collections.deque(paths)
+    paths = set(backup.generate_paths())
+    assert paths == set(expected_paths)

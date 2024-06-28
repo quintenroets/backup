f9f9a0ac4726a99c80816c79ea7c0bd1f9cdd5be
Quinten <quinten.roets@gmail.com>
2024-06-28 00:04:52 -0700

    test substitute

--- tests/backups/test_commands_backup.py
+++ tests/backups/test_commands_backup.py
@@ -8,6 +8,7 @@ def test_substitute_username() -> None:
     key = "USERNAME"
     value = "name"
     os.environ[key] = "name"
-    path_str = commands.Backup.substitute_correct_username(Path.HOME)
-    assert path_str == str(Path.HOME.with_name(value))
+    path = Path.remote / Path.HOME.relative_to(Path("/"))
+    path_str = commands.Backup.substitute_correct_username(path)
+    assert Path(path_str).name == value
     os.environ.pop(key)

8d0b7dd7fbdcc32d6f9c99cb054858fd5390a122
Quinten <quinten.roets@gmail.com>
2024-06-30 02:19:04 +0000

    use ruff best practices (#27)

--- src/backup/cli/entry_point.py
+++ src/backup/cli/entry_point.py
@@ -1,7 +1,6 @@
 from package_utils.context.entry_point import create_entry_point
 
-from backup.main.main import main
-
-from ..context import context
+from backup import main
+from backup.context import context
 
 entry_point = create_entry_point(main, context)

b61823377155dcbac454c5ee0e545bef212d14e7
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2025-09-24 09:47:22 -0700

    add common sync configs (#64)
    * add common sync configs
    
    * better name

---
+++ src/backup/syncer/sync_configs.py
@@ -0,0 +1,12 @@
+from backup.models import Path
+
+from .sync_config import SyncConfig
+
+
+class SyncConfigs:
+    home = SyncConfig(source=Path.HOME, dest=Path.remote / "home")
+    cache = SyncConfig(source=Path.backup_source, dest=Path.backup_cache)
+
+
+def select_sync_config(path: Path) -> SyncConfig:
+    return SyncConfigs.home if path.is_relative_to(Path.HOME) else SyncConfig()

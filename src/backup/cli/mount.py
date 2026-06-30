8268904fe69f7d16aa7be6e56ade51d0a68da24b
Quinten <quinten.roets@gmail.com>
2026-06-29 20:39:07 -0700

    remove mounting functionality

--- src/backup/cli/mount.py
+++
@@ -1,5 +0,0 @@
-from package_utils.cli import create_entry_point
-
-from backup.main.mount import Mounter
-
-entry_point = create_entry_point(Mounter.run, Mounter)
--- tests/test_cli_entry_points.py
+++ tests/test_cli_entry_points.py
@@ -1,9 +1,8 @@
 from unittest.mock import MagicMock, patch
 
-from package_dev_utils.tests.args import cli_args, no_cli_args
-from superpathlib import Path
+from package_dev_utils.tests.args import no_cli_args
 
-from backup.cli import entry_point, mount
+from backup.cli import entry_point
 
 
 @no_cli_args
@@ -11,22 +10,3 @@ from backup.cli import entry_point, mount
 def test_entry_point(mocked_run: MagicMock) -> None:
     entry_point.entry_point()
     mocked_run.assert_called_once()
-
-
-@patch("cli.launch")
-@patch("cli.run")
-def test_mount_entry_point(mocked_run: MagicMock, mocked_launch: MagicMock) -> None:
-    path = Path.tempfile(create=False)
-    args = cli_args("--path", path / "subfolder")
-    with path, args:
-        mount.entry_point()
-    mocked_run.assert_called()
-    mocked_launch.assert_called_once()
-
-
-@no_cli_args
-@patch("cli.launch")
-def test_mount_entry_point_without_path_specified(mocked_launch: MagicMock) -> None:
-    with patch("cli.run"):
-        mount.entry_point()
-    mocked_launch.assert_called_once()

574d59f8b6841eae3e8e0d31227e8be51ec562fb
Quinten <quinten.roets@gmail.com>
2026-06-30 19:48:46 -0700

    improve annotations

--- src/backup/context.py
+++ src/backup/context.py
@@ -43,10 +43,10 @@ class Config:
     overwrite_newer: bool = True
     retries: int = 5
     n_checkers: int = 100
-    n_parallel_transfers = 100
+    n_parallel_transfers: int = 100
     retries_sleep: str = "30s"
     order_by: str = "size,desc"  # handle largest files first
-    drive_import_formats = "docx, xlsx"
+    drive_import_formats: str = "docx, xlsx"
     max_backup_size: int = int(50e6)
 
 
